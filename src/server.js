6bae31bba5c27831ac1ff6284ab4d4f547011e96
Nicholas Cato <nac5961@g.rit.edu>
2018-10-01 18:03:26 -0400

    Fixed a problem with eslint

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-//Requires
+// Requires
 const http = require('http');
 const url = require('url');
 const htmlHandler = require('./htmlResponses.js');
@@ -6,45 +6,42 @@ const ajaxHandler = require('./ajaxResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-//Variable to process the URL
+// Variable to process the URL
 const urlHandler = {
-	GET: {
-		'/': htmlHandler.getIndex,
-		'/style.css': htmlHandler.getCSS,
-		'/bundle.js': htmlHandler.getBundle,
-		'/getGames': ajaxHandler.getGames,
-		defaultResponse: ajaxHandler.notFound
-	},
-	HEAD: {
-		'/getGames': ajaxHandler.getGamesMeta
-	},
-	POST: {
-		'/createGame': ajaxHandler.addGame,
-	}
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/bundle.js': htmlHandler.getBundle,
+    '/getGames': ajaxHandler.getGames,
+    defaultResponse: ajaxHandler.notFound,
+  },
+  HEAD: {
+    '/getGames': ajaxHandler.getGamesMeta,
+  },
+  POST: {
+    '/createGame': ajaxHandler.addGame,
+  },
 };
 
-//Processes all of the requests to the server
+// Processes all of the requests to the server
 const onRequest = (request, response) => {
-	//Parse URL
-	const parsedURL = url.parse(request.url);
-	
-	//Get method in the request (GET/HEAD/POST)
-	const { method } = request;
-	
-	console.dir(parsedURL);
-	
-	//Process the method
-	if (urlHandler[method] && urlHandler[method][parsedURL.pathname]){
-		urlHandler[method][parsedURL.pathname](request, response, parsedURL.query);
-	}
-	else{
-		urlHandler.GET.defaultResponse(request, response);
-	}
+  // Parse URL
+  const parsedURL = url.parse(request.url);
+
+  // Get method in the request (GET/HEAD/POST)
+  const { method } = request;
+
+  console.dir(parsedURL);
+
+  // Process the method
+  if (urlHandler[method] && urlHandler[method][parsedURL.pathname]) {
+    urlHandler[method][parsedURL.pathname](request, response, parsedURL.query);
+  } else {
+    urlHandler.GET.defaultResponse(request, response);
+  }
 };
 
-//Start server
+// Start server
 http.createServer(onRequest).listen(port);
 
 console.log('Server started');
-
-
--- src/utilities.js
+++ src/utilities.js
@@ -1,38 +1,35 @@
-//Helper function used for sorting the games
+// Helper function used for sorting the games
 const compare = (a, b) => {
-	
-	const nameA = a.name;
-	const nameB = b.name;
-	
-	//a comes before b
-	if (nameA > nameB){
-		return 1;
-	}
-	
-	//b comes before a
-	else if (nameA < nameB){
-		return -1;
-	}
-	
-	//same
-	return 0;
+  const nameA = a.name;
+  const nameB = b.name;
+
+  // a comes before b
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  // b comes before a
+  if (nameA < nameB) {
+    return -1;
+  }
+
+  // same
+  return 0;
 };
 
-//Helper function used for reverse sorting the games
-const reverseCompare = (a, b) => {
-	return compare(a, b) * -1;
-};
+// Helper function used for reverse sorting the games
+const reverseCompare = (a, b) => compare(a, b) * -1;
 
-//Function to sort the games
+// Function to sort the games
 const sortGamesArr = (games) => {
-	games.sort(compare);
+  games.sort(compare);
 };
 
-//Function to reverse sort the games
+// Function to reverse sort the games
 const reverseSortGamesArr = (games) => {
-	games.sort(reverseCompare);
+  games.sort(reverseCompare);
 };
 
-//Export the functions (make them public)
+// Export the functions (make them public)
 module.exports.sortGamesArr = sortGamesArr;
 module.exports.reverseSortGamesArr = reverseSortGamesArr;
